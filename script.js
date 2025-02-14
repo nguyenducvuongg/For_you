521e62751fcedb6c8270274f737182b8bab3584d
nguyenducvuong <ducvuongnguyen098@gmail.com>
2025-02-14 10:43:19 +0700

    fix0.3

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const cursor = document.querySelector(".heart");
-function updateCursor(x, y) {
-    cursor.style.left = x - 145 + "px";
-    cursor.style.top = y - 145 + "px";
+const desktopOffset = { x: 100, y: 100 };
+const mobileOffset = { x: 150, y: 150 };
+function updateCursorPosition(x, y, offsets) {
+  cursor.style.left = (x - offsets.x) + "px";
+  cursor.style.top = (y - offsets.y) + "px";
 }
-window.addEventListener("mousemove", function(e) {
-    updateCursor(e.x, e.y);
+window.addEventListener("mousemove", function (e) {
+  updateCursorPosition(e.clientX, e.clientY, desktopOffset);
 });
-window.addEventListener("touchmove", function(e) {
-    if (e.touches && e.touches.length > 0) {
-        const touch = e.touches[0];
-        updateCursor(touch.clientX, touch.clientY);
-    }
-    e.preventDefault();
-},{passive: false});
+window.addEventListener("touchmove", function (e) {
+  if (e.touches && e.touches.length > 0) {
+    updateCursorPosition(e.touches[0].clientX, e.touches[0].clientY, mobileOffset);
+  }
+  e.preventDefault();
+}, { passive: false });
